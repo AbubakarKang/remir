24f3ec91da3bb30771173296b0e7c7146df1baff
AbubakarKang <90056797+AbubakarKang@users.noreply.github.com>
2022-04-19 17:06:41 +0800

    💩 Reminder addition (PROTOTYPE)

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 //----------------------------\\ DOM ELEMENTS //----------------------------\\
 
 const closeBtn = document.querySelector("[data-close-btn]");
+const addButton = document.querySelector("[data-add-button]");
+const todosList = document.querySelector("[data-todos-list]");
 const minimizeBtn = document.querySelector("[data-minimize-btn]");
 const maximizeBtn = document.querySelector("[data-maximize-btn]");
+const additionInput = document.querySelector("[data-addition-input]");
 
 //-------------------------------\\ OTHERS //-------------------------------\\
 
@@ -14,3 +17,16 @@ const ipc = ipcRenderer;
 closeBtn.addEventListener("click", () => ipc.send("closeApp"));
 minimizeBtn.addEventListener("click", () => ipc.send("minimizeApp"));
 maximizeBtn.addEventListener("click", () => ipc.send("maximizeApp"));
+
+addButton.addEventListener("click", () => {
+	let todoText = additionInput.value;
+	todosList.innerHTML += `<div class="todo">
+    <div class="todo-detail">
+        <div class="todo-text">${todoText}</div>
+        <div class="todo-time"></div>
+    </div>
+    <div class="todo-delete">
+        <i class="fa-solid fa-trash"></i>
+    </div>
+</div>`;
+});
